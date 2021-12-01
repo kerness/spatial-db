14e2e1d2af11171a13e7b4b2ff5cdcba0765a2f6
kerness <maciej.bak260@gmail.com>
2021-12-01 16:24:20 +0100

    cleaning

--- lab4/lab4.sql
+++ lab4/lab4.sql
@@ -18,6 +18,7 @@ insert into geoobiekty.obiekty values
 
 select * from geoobiekty.obiekty;
 
+
 -- 1:
 
 select st_area(st_buffer(st_shortestline(
@@ -25,17 +26,16 @@ select st_area(st_buffer(st_shortestline(
     (select o.geom from geoobiekty.obiekty o where o.name = 'obiekt4')
 ), 5));
 
--- 2: convert 'obiekt4' to polygon - so the line endings in 'obiekt4' must be closed!
+-- 2: convert 'obiekt4' to polygon - the line endings in 'obiekt4' must be closed!
 
 -- convert to multicurve to linestring
+-- Returns a (set of) LineString(s) formed by sewing together the constituent line work of a MULTILINESTRING. 
 select st_geometrytype(st_linemerge(st_curvetoline(geom))) from geoobiekty.obiekty where name = 'obiekt4';
-
-
--- convert to closed linestring
-
--- st_addpoint - adds a point to a LineString before point
+-- convert to closed linestring. st_addpoint - adds a point to a LineString so the line cn be closed by makepolygon
 select st_addpoint(st_linemerge(st_curvetoline(geom)), st_startpoint(st_linemerge(st_curvetoline(geom)))) into tmpTable
 from geoobiekty.obiekty where name = 'obiekt4';
+
+
 
 -- convert to polygon
 select st_makepolygon(st_addpoint(st_linemerge(st_curvetoline(geom)), st_startpoint(st_linemerge(st_curvetoline(geom))))) 
